dce9e2db7927d63f8f285864814124e8a8c8244b
mscamarilla <persocom01@gmail.com>
2020-03-19 17:11:38 +0200

    task 4 option 2

--- task_1/task_4.js
+++ task_1/task_4.js
@@ -8,7 +8,7 @@ count(arr, 20, 100); // 6   (подходят элементы 21,23,24,64,70,84
 count(arr, 1000, 2000); // 5   (подходят элементы 1099, 1200, 1201, 1202, 2000)
 count(arr, 84, 84); // 1   (подходит элемент 84)
 
-
+//O(n)
 function count1(arr, l, r) {
 
     let res = [];
@@ -19,5 +19,48 @@ function count1(arr, l, r) {
     }
 
     console.log(res.length);
+
+}
+
+//O(logN)
+function count(arr, left, right) {
+    let l = 0;
+    let r = arr.length - 1;
+    let leftIndex;
+    let rightIndex;
+
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2);
+
+        if (arr[m] >= left) {
+            leftIndex = m;
+        }
+
+        if (arr[m] < left) {
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+
+    }
+
+    l = 0;
+    r = arr.length - 1;
+
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2);
+
+        if (arr[m] <= right) {
+            rightIndex = m;
+        }
+
+        if (arr[m] < right) {
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+
+    }
+    console.log(rightIndex - leftIndex + 1);
 
 }
