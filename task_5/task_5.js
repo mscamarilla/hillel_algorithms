052466ef6eecc4b48bc453a4d1cef55d2841e495
mscamarilla <persocom01@gmail.com>
2020-05-16 00:01:23 +0300

    rework

--- task_5/task_5.js
+++ task_5/task_5.js
@@ -4,48 +4,50 @@
 function grains(field) {
     const n = field.length; // строки
     const m = field[0].length; // столбцы
-    field.reverse(); //реверс
 
-    let temp_arr = [];
-//создаем вспомогательный массив и заполняем его -1
+    let dp = [];
+    //создаем вспомогательный массив и заполняем его -1
     for (let i = 0; i <= n + 1; i++) {
-        temp_arr.push(Array(m + 2).fill(-1));
+        dp.push(Array(m + 2).fill(-1));
     }
-    temp_arr[0][1] = 0;//реверс
+
+    dp[n + 1][1] = 0;
 
     //во вспомогательный массив копируем исходный. Так создадутся стенки
-    for (let i = 0; i < n; i++) {
+    for (let i = n; i > 0; i--) {
         for (let j = 0; j < m; j++) {
-            temp_arr[i + 1][j + 1] = field[i][j] + Math.max(temp_arr[i][j + 1], temp_arr[i + 1][j]);
+            dp[i][j + 1] = field[i - 1][j] + Math.max(dp[i][j], dp[i + 1][j + 1]);
         }
     }
-    console.log(temp_arr)
+    console.log(dp);
     let result = '';
-    let i = 1;
+    let i = n;
     let j = 1;
-    while (i <= n && j <= m) {
+
+    while (i > 0 && j <= m) {
         //если значение сверху больше (или равно), чем справа, двигаемся вверх
-        if (temp_arr[i + 1][j] >= temp_arr[i][j + 1]) {
-            if (temp_arr[i + 1][j] !== -1) {
+        if (dp[i - 1][j] >= dp[i][j + 1]) {
+            if (dp[i - 1][j] !== -1) {
                 result += 'F';
             }
-            i++;
+            i--;
 
         } else {
             //если значение справа больше, чем вверху, двигаемся вправо
-            if (temp_arr[i][j + 1] !== -1) {
+            if (dp[i][j + 1] !== -1) {
                 result += 'R';
             }
             j++;
 
         }
     }
-    return result.split("").reverse().join("");
+
+    return result;
 }
 
 let field = [
-    [3, 2, 4],
-    [1, 5, 1],
+    [50, 10, 100],
+    [1, 10, 40],
 ]; //RFR
 
 
